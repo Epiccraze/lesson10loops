a112c21f450f3d47259f0a8db8fb72aa9453ad9c
Epiccraze <Pal_rao@yahoo.com>
2025-07-17 16:33:26 -0500

    Add files via upload

--- lesson10loops.py
+++ lesson10loops.py
@@ -3,3 +3,15 @@ sum=0
 for i in range(1, number+1):
     sum=sum+i
     print("/n The result is",sum)
+
+string=input("Enter a word")
+string2=(" ")
+for i in string:
+    string2=i+string2
+    print("/n The original word is ",string)
+    print("/n The reversed word is ",string2)
+
+num=int (input("Enter a number"))
+print("Numbers from 5-1" .format(num,1))
+for i in range(num,0,-1):
+    print(i)
